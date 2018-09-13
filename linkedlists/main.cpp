d4d7f09960da9504ab86a04a6f1ba3158d9398dd
MinerK <s3rserafim@gmail.com>
2018-09-13 15:02:25 +0300

    Изменения с пары

--- linkedlists/main.cpp
+++ linkedlists/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 struct Node
 {
-	int value;
-	Node* next;
-	Node* prev;
+    int value;
+    Node* next;
+    Node* prev;
 };
 
 void add(Node*& first, int value);
@@ -14,116 +14,123 @@ int value(Node *first, int index);
 void remove(Node *&first, int index = 0);
 int length(Node* first);
 void PrintList(Node * first);
+void DeleteList(Node*& first);
 
 int main()
 {
-	int x;
-	cout << "Enter next value, 0 to stop: "; cin >> x;
+    int x;
+    cout << "Enter next value, 0 to stop: "; cin >> x;
+    
+    Node *List1 = nullptr;
+    
+    while (x!=0)
+    {
+        add(List1, x);
+        cout << "Enter next value, 0 to stop: "; cin >> x;
+    }
+    
+    cout << "Length of the list: " << length(List1) << endl;
+    
+    PrintList(List1);
+    
 
-	Node *List1 = nullptr;
-
-	while (x!=0)
-	{
-		add(List1, x);
-		cout << "Enter next value, 0 to stop: "; cin >> x;
-	}
-
-	cout << "Length of the list: " << length(List1) << endl;
-
-	PrintList(List1);
-
-	int index_to_del;
-	cout << "Index of a node to be deleted: "; cin >> index_to_del;
-	remove(List1, index_to_del);
-
-	PrintList(List1);
-
-	system("pause");
-	return 0;
+    int index_to_del;
+    cout << "Index of a node to be deleted: "; cin >> index_to_del;
+    remove(List1, index_to_del);
+    
+    PrintList(List1);
+    
+    DeleteList(List1);
+    
+    if(List1==nullptr)
+        cout<<"List was deleted."<<endl;
+    
+    system("pause");
+    return 0;
 }
 
 void add(Node *& first, int value)
 {
-	Node* newnode = new Node;
-	newnode->next = nullptr;
-	newnode->prev = nullptr;
-	newnode->value = value;
-	if (first==nullptr)
-	{
-		first = newnode;
-	}
-	else
-	{
-		if (first->next == nullptr)
-		{
-			newnode->next = first;
-			newnode->prev = first;
-			first->next = newnode;
-			first->prev = newnode;
-			
-		}
-		else
-		{
-			Node* last = first;
-			while (last->next != first)
-			{
-				last = last->next;
-			}
-			newnode->next = first;
-			newnode->prev = last;
-			last->next = newnode;
-			first->prev = newnode;
-		}
-	}
+    Node* newnode = new Node;
+    newnode->next = newnode;
+    newnode->prev = newnode;
+    newnode->value = value;
+    if (first==nullptr)
+    {
+        first = newnode;
+    }
+    else
+    {
+        Node* last = first->prev;
+        newnode->next = first;
+        newnode->prev = last;
+        last->next = newnode;
+        first->prev = newnode;
+
+    }
 }
 
 int value(Node * first, int index)
 {
-	Node *cursor = first;
-	for (int i = 0; i < index ; cursor=cursor->next, i++);
-	return cursor->value;
+    Node *cursor = first;
+    for (int i = 0; i < index ; cursor=cursor->next, i++);
+    return cursor->value;
 }
 
 void remove(Node *& first, int index)
 {
-	if(index == 0)
-	{
-		if (first->next != nullptr && first->prev != nullptr)
-		{
-			(first->prev)->next = first->next;
-			(first->next)->prev = first->prev;
-			Node* tmp = first->next;
-			delete[] first;
-			first = tmp;
-		}
-	}
-	else
-	{
-		Node *cursor = first;
-		for (int i = 0; i < index; cursor = cursor->next, i++);
-		(cursor->prev)->next = cursor->next;
-		(cursor->next)->prev = cursor->prev;
-		delete[] cursor;
-	}
+    if (first->next==first && first->prev==first)
+    {
+            delete[] first;
+            first=nullptr;
+    }
+    else
+    {
+    Node *cursor = first;
+    for (int i = 0; i < index; cursor = cursor->next, i++);
+    (cursor->prev)->next = cursor->next;
+    (cursor->next)->prev = cursor->prev;
+        if (cursor==first)
+        {
+            Node* tmp = first->next;
+            delete[] first;
+            first = tmp;
+        }
+        else
+        {
+            delete[] cursor;
+        }
+    }
 }
 
 int length(Node * first)
 {
-	Node *cursor = first;
-	int i = 0;
-	while (cursor != nullptr && cursor->next != first)
-	{
-		cursor = cursor->next;
-		i++;
-	}
-	return i;
+    if(first==nullptr)
+        return 0;
+    Node *cursor = first;
+    int i = 1;
+    while (cursor->next != first)
+    {
+        cursor = cursor->next;
+        i++;
+    }
+    return i;
 }
 
 void PrintList(Node * first)
 {
-	int n = length(first);
-	for (int i = 0; i <= n; i++)
-	{
-		cout<< i << " : "<< value(first, i)<< endl;
-	}
+    int n = length(first);
+    for (int i = 0; i < n; i++)
+    {
+        cout<< i << " : "<< value(first, i)<< endl;
+    }
+}
+
+void DeleteList(Node *& first)
+{
+    int n=length(first);
+    for (int i=n-1; i>=0; i--) {
+        remove(first, 0);
+    }
+    
 }
