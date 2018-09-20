9a3f1b8220ca35348fe70e727a4134f6a6d31200
MinerK <s3rserafim@gmail.com>
2018-09-20 16:36:29 +0300

    para

--- linkedlists/main.cpp
+++ linkedlists/main.cpp
@@ -1,4 +1,5 @@
 #include "List.h"
+#include <iostream>
 using namespace std;
 
 int main()
@@ -6,7 +7,7 @@ int main()
     int x;
     cout << "Enter next value, 0 to stop: "; cin >> x;
     
-	List list1;
+    List list1;
     
     while (x!=0)
     {
@@ -18,12 +19,23 @@ int main()
     
     list1.Print();
     
-
+    
     int index_to_del;
     cout << "Index of a node to be deleted: "; cin >> index_to_del;
     list1.remove(index_to_del);
     
-    list1.Print();
+    int n = list1.length();
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " : " << list1[i] << endl;
+    }
+    bool asc;
+    cout<<"Ascending sort? 0=false: ";
+    cin>>asc;
+    List list2 = list1.sorted(asc);
+    list2.Print();
+
+    
     
     system("pause");
     return 0;
